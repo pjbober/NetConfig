4f836151be7009e11d6797e350236f808251b7d0
Piotr <pjbober@gmail.com>
2013-11-16 19:24:02 +0100

    Clicking on Profile name

---
+++ ASK/ViewModels/NetsList/NetInterfaceViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using ASK.Model.NetsList;
+
+namespace ASK.ViewModels.NetsList
+{
+    class NetInterfaceViewModel
+    {
+        public NetInterfaceViewModel(NetInterface netInterface)
+        {
+            NetInterfaceModel = netInterface;
+            Profiles = new ObservableCollection<ProfileViewModel>();
+            foreach (Profile profile in netInterface.Profiles)
+                Profiles.Add(new ProfileViewModel(profile));
+        }
+
+        public ObservableCollection<ProfileViewModel> Profiles { get; set; }
+
+        public NetInterface NetInterfaceModel { get; set; }
+    }
+}
---
+++ ASK/ViewModels/NetsList/ProfileViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ASK.Model.NetsList;
+using System.Windows.Input;
+
+namespace ASK.ViewModels.NetsList
+{
+    class ProfileViewModel
+    {
+        public ProfileViewModel(Profile profile)
+        {
+            ProfileModel = profile;
+            BtnCliked = new MyCommand();
+        }
+
+        public Profile ProfileModel { get; set; }
+
+        public ICommand BtnCliked
+        {
+            get;
+            set;
+        }
+    }
+}
+
+class MyCommand : ICommand
+{
+    public bool CanExecute(object param)
+    {
+        return true;
+    }
+
+    public void Execute(object param)
+    {
+        System.Windows.MessageBox.Show("Not yet implemented");
+    }
+
+    public event EventHandler CanExecuteChanged;
+}
