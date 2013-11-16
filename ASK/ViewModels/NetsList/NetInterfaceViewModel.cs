cce4323f4686b06ee239d27821116f6bea68f58c
Piotr <pjbober@gmail.com>
2013-11-16 21:24:07 +0100

    refactor view model

--- ASK/ViewModels/NetsList/NetInterfaceViewModel.cs
+++ ASK/ViewModels/NetsList/NetInterfaceViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Collections.ObjectModel;
 using ASK.Model.NetsList;
+using System.Windows.Input;
 
 namespace ASK.ViewModels.NetsList
 {
@@ -12,13 +13,49 @@ namespace ASK.ViewModels.NetsList
         public NetInterfaceViewModel(NetInterface netInterface)
         {
             NetInterfaceModel = netInterface;
-            Profiles = new ObservableCollection<ProfileViewModel>();
+            BtnClicked = new CommandHandler(x => Clicked(x), true);
+            Profiles = new ObservableCollection<Profile>();
             foreach (Profile profile in netInterface.Profiles)
-                Profiles.Add(new ProfileViewModel(profile));
+                Profiles.Add(profile);
         }
 
-        public ObservableCollection<ProfileViewModel> Profiles { get; set; }
+        public ObservableCollection<Profile> Profiles { get; set; }
 
         public NetInterface NetInterfaceModel { get; set; }
+        public void Clicked(object obj)
+        {
+            Profile profile = obj as Profile;
+            System.Windows.MessageBox.Show("You've chosen profile " + profile.Name);
+        }
+
+        public ICommand BtnClicked
+        {
+            get;
+            set;
+        }
     }
 }
+
+public class CommandHandler : ICommand
+{
+    private Action<object> _action;
+    private bool _canExecute;
+    public CommandHandler(Action<object> action, bool canExecute)
+    {
+        _action = action;
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        return _canExecute;
+    }
+
+    public event EventHandler CanExecuteChanged;
+
+    public void Execute(object parameter)
+    {
+        _action(parameter);
+    }
+}
+
--- ASK/ViewModels/OptionsControl/DummyOptionsControlViewModel.cs
+++ ASK/ViewModels/OptionsControl/DummyOptionsControlViewModel.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ASK.Model.NetsList;
 
 namespace ASK.ViewModels.OptionsControl
 {
     class DummyOptionsControlViewModel
     {
         public String ProfileName { get; set; }
+
+        public void OnProfileChanged(Profile newProfile)
+        {
+            ProfileName = newProfile.Name;
+        }
     }
 }
