a386c0a0f277e50eb1527868e382af83314b82e3
Jakub Liput <jakub.liput@gmail.com>
2013-11-28 21:07:59 +0100

    Język polski dla netsh

--- ASK/Logic/WifiInterfacesParser.cs
+++ ASK/Logic/WifiInterfacesParser.cs
@@ -9,6 +9,9 @@ namespace ASK.Logic
 {
     class WifiInterfacesParser
     {
+        // TODO: To na razie nie działa, trzeba wymyślić jakiś inny sposób
+        private static readonly Encoding ibm852enc = Encoding.GetEncoding("ibm852");
+
         public List<Profile> parse()
         {
             string netshOutput = invokeNetsh();
@@ -16,12 +19,12 @@ namespace ASK.Logic
             List<Profile> profiles = new List<Profile>();
             List<string> outputLines = netshOutput.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).Where(p => p.Contains(":") && !p.StartsWith("There") && !p.StartsWith("Hosted")).ToList();
             List<Tuple<string, string>> oneProfile = new List<Tuple<string, string>>();
-
+            
             foreach (string line in outputLines)
             {
                 List<string> newLine = line.Split(new char[] { ':' }, 2).Select(p => p.Trim()).ToList();
                 oneProfile.Add(new Tuple<string, string>(newLine[0], newLine[1]));
-                if (newLine[0] == "Profile")
+                if (newLine[0].StartsWith("Stan sieci"))
                 {
                     profiles.Add(makeProfile(oneProfile.ToDictionary(x => x.Item1, x => x.Item2)    ));
                     oneProfile.Clear();
@@ -33,23 +36,49 @@ namespace ASK.Logic
 
         private Profile makeProfile(Dictionary<string, string> profileOptions)
         {
-            Profile profile = new Profile(profileOptions["Name"]);
-            profile.PhysicalAddress = profileOptions["Physical address"];
-            profile.GUID = profileOptions["GUID"];
-            profile.Authentication = profileOptions["Authentication"];
-            profile.BSSID = profileOptions["BSSID"];
-            profile.Channel = profileOptions["Channel"];
-            profile.Cipher = profileOptions["Cipher"];
-            profile.ConnectionMode = profileOptions["Connection mode"];
-            profile.Description = profileOptions["Description"];
-            profile.NetworkType = profileOptions["Network type"];
-            profile.ProfileName = profileOptions["Profile"];
-            profile.RadioType = profileOptions["Radio type"];
-            profile.ReceiveRate = profileOptions["Receive rate (Mbps)"];
-            profile.Signal = profileOptions["Signal"];
-            profile.SSID = profileOptions["SSID"];
-            profile.State = profileOptions["State"];
-            profile.TransmitRate = profileOptions["Transmit rate (Mbps)"];
+            String profileName =
+                Encoding.UTF8.GetString(ibm852enc.GetBytes(profileOptions["Nazwa"]));
+            
+            Profile profile = new Profile(profileName);
+            profile.PhysicalAddress = profileOptions["Adres fizyczny"];
+            profile.GUID = profileOptions["Identyfikator GUID"];
+
+            // TODO: tylko jak jest połączony
+            //profile.Signal = profileOptions[profileOptions.Keys.Where(x => x.StartsWith("Sygn")).ToList()[0]];
+            //profile.SSID = profileOptions["SSID"];
+            //profile.Authentication = profileOptions["Uwierzytelnianie"];
+
+            //Profile profile = new Profile(profileOptions["Name"]);
+            //profile.PhysicalAddress = profileOptions["Physical address"];
+            //profile.GUID = profileOptions["GUID"];
+            //profile.Authentication = profileOptions["Authentication"];
+            //profile.BSSID = profileOptions["BSSID"];
+            //profile.Channel = profileOptions["Channel"];
+            //profile.Cipher = profileOptions["Cipher"];
+            //profile.ConnectionMode = profileOptions["Connection mode"];
+            //profile.Description = profileOptions["Description"];
+            //profile.NetworkType = profileOptions["Network type"];
+            //profile.ProfileName = profileOptions["Profile"];
+            //profile.RadioType = profileOptions["Radio type"];
+            //profile.ReceiveRate = profileOptions["Receive rate (Mbps)"];
+            //profile.Signal = profileOptions["Signal"];
+            //profile.SSID = profileOptions["SSID"];
+            //profile.State = profileOptions["State"];
+            //profile.TransmitRate = profileOptions["Transmit rate (Mbps)"];
+            
+            //profile.BSSID = profileOptions["BSSID"];
+            //profile.Channel = profileOptions["Channel"];
+            //profile.Cipher = profileOptions["Cipher"];
+            //profile.ConnectionMode = profileOptions["Connection mode"];
+            //profile.Description = profileOptions["Description"];
+            //profile.NetworkType = profileOptions["Network type"];
+            //profile.ProfileName = profileOptions["Profile"];
+            //profile.RadioType = profileOptions["Radio type"];
+            //profile.ReceiveRate = profileOptions["Receive rate (Mbps)"];
+
+            //profile.State = profileOptions["State"];
+            //profile.TransmitRate = profileOptions["Transmit rate (Mbps)"];
+
             return profile;
         }
 
